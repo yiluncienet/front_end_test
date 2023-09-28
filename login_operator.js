818df9381e5961d23e807b3617fac31e2aa1a209
yiluncienet <yilun.wu@cienet.com.cn>
2023-09-28 13:22:00 +0800

    Allen [Build] Built up login as operator

---
+++ login_operator.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         login_operator
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://localhost:8233/login.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.localhost
+// @require      https://raw.githubusercontent.com/yiluncienet/front_end_test/master/esim.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     esim.start();
+     esim.input('#username','yilun.wu.op');
+     esim.input('#password','Welcome@123456');
+     esim.click('#button');
+    // Your code here...
+})();
