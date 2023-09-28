38d6ada7b2d12915f0aa78e4de8d1e5b1bfa18d9
Yilun Wu (EXT) <yilun.wu.ext@ericsson.com>
2023-09-28 10:58:44 +0800

    Allen [built] built up model to simulate the front-end action

---
+++ esim.js
@@ -0,0 +1,24 @@
+// esim.js
+var esim = (function() {
+    
+    
+    function start() {
+        alert('Attention! The script will start');
+    }
+
+    // function multiply(a, b) {
+    //     return a * b;
+    // }
+
+
+    function end() {
+        alert('Attention! The script done');
+    }
+
+
+    return {
+        start: start,
+        // multiply: multiply
+        end: end,
+    };
+})();
