e8017f53b5719824422d34d768a8a99d32ccfaee
yiluncienet <yilun.wu@cienet.com.cn>
2023-09-28 12:26:14 +0800

    Allen [build] built up Login as sec_admin

---
+++ login_sec_admin.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://localhost:8233/login.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.localhost
+// @require      https://raw.githubusercontent.com/yiluncienet/front_end_test/master/esim.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     esim.start();
+     esim.input('#username','sec_admin');
+     esim.input('#password','AdminAdmin123!');
+     esim.button('#button');
+    // Your code here...
+})();
