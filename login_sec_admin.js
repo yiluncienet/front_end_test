e532186bd4e389b66d596da7d05e68cd36c8cbcc
yiluncienet <yilun.wu@cienet.com.cn>
2023-09-30 15:59:41 +0800

    Allen[Update] Added settings

--- login_sec_admin.js
+++ login_sec_admin.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         login_sec_admin
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://localhost:8233/login.html
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.localhost
 // @require      https://raw.githubusercontent.com/yiluncienet/front_end_test/master/esim.js
 // @homepageURL  https://github.com/yiluncienet/front_end_test
+// @downloadURL  https://raw.githubusercontent.com/yiluncienet/front_end_test/master/login_sec_admin.js
 // @grant        none
 // ==/UserScript==
 
