0c6eedda9b96f3d717fed5bd002c809721f718d7
yiluncienet <yilun.wu@cienet.com.cn>
2023-09-30 15:52:11 +0800

    Allen [Update] Add settings and remove useless codes

--- login_sec_admin.js
+++ login_sec_admin.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         login_sec_admin
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://localhost:8233/login.html
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.localhost
 // @require      https://raw.githubusercontent.com/yiluncienet/front_end_test/master/esim.js
+// @homepageURL  https://github.com/yiluncienet/front_end_test
 // @grant        none
 // ==/UserScript==
 
@@ -16,5 +17,4 @@
      esim.input('#username','sec_admin');
      esim.input('#password','AdminAdmin123!');
      esim.click('#button');
-    // Your code here...
 })();
